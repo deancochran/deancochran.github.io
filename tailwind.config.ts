d09c8100121e1c8af2ca08b8c83002600af005cd
Dean Cochran <charles.dean.cochran@outlook.com>
2024-10-21 07:29:13 -0700

    npm update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,23 @@
-import { skeleton } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-import forms from '@tailwindcss/forms';
-import { join } from 'path';
-import type { Config } from 'tailwindcss';
+import { skeleton } from '@skeletonlabs/skeleton/plugin'
+import * as themes from '@skeletonlabs/skeleton/themes'
+import forms from '@tailwindcss/forms'
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
 export default {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
+		join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}'),
 	],
 
 	theme: {
-		extend: {}
+		extend: {},
 	},
 
 	plugins: [
 		forms,
 		skeleton({
-			themes: [themes.cerberus, themes.catppuccin, themes.pine, themes.rose]
-		})
-	]
-} as Config;
+			themes: [themes.cerberus, themes.catppuccin, themes.pine, themes.rose],
+		}),
+	],
+} as Config
